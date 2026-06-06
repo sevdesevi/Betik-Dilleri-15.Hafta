8161b8c7c56b0922c40133154a6bcef0e3887119
sevdesevi <svsila16@gmail.com>
2026-06-07 00:07:26 +0300

    Update RusRuleti_3.py

--- RusRuleti_3.py
+++ RusRuleti_3.py
@@ -6,7 +6,7 @@ for i in range(6):
     sec =input("secilecek mahkum adını oku")
     kurban_listesi.append(sec)
     print("{} sahneye çıkarıldı".format(sec))
-silah_patladi=0 #BURAK ŞAHİN TARAFINDAN YAZILDI
+silah_patladi=0 
 sayac=1 #çünkü sayac indisi tutmuyor
 r=random.randint(1,6)
 print("ss subayı silahını çıkardı ve rus ruleti için hazırladı.")
